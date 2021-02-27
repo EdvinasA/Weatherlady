cbc6d84187e17929005688517feeb5a7e562190a
Edvinas <edvinasalimas98@gmail.com>
2021-02-27 11:32:39 +0200

    Location

--- src/main/java/com/Weatherlady/Project.java
+++ src/main/java/com/Weatherlady/Project.java
@@ -1,8 +1,31 @@
 package com.Weatherlady;
 
+import com.Weatherlady.application.Location;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
 public class Project {
 
     public void run(){
+        SessionFactory sessionFactory = new Configuration()
+                .configure("persistence.cfg.xml")
+                .addAnnotatedClass(Location.class)
+                .buildSessionFactory();
+
+        EntityManager entityManager = sessionFactory.createEntityManager();
+
+        EntityTransaction transaction = entityManager.getTransaction();
+
+            transaction.begin();
+
+            Location location = new Location("a");
+
+            entityManager.persist(location);
+            transaction.commit();
+            entityManager.close();
 
     }
 }
---
+++ src/main/java/com/Weatherlady/application/Location.java
@@ -0,0 +1,70 @@
+package com.Weatherlady.application;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Locations")
+public class Location {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        Long id;
+        String locationName;
+        String longitude;
+        String latitude;
+        String region;
+        String countryName;
+
+        public Location(String locationName) {
+            this.locationName = locationName;
+        }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+}
+
